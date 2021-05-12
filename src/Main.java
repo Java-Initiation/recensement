94b0359e9d784e92b846ecb1308efcb8872a10d9
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2021-05-12 17:00:18 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Path path = Paths.get("./recensement.csv");
+        Path pathEcriture = Paths.get("./recensement2.csv");
+
+        try {
+            List<String> lines = Files.readAllLines(path);
+            ArrayList<String> newDatas = new ArrayList<>();
+            Iterator<String> iterator = lines.iterator();
+            iterator.next();
+            while (iterator.hasNext()){
+                String[] columns = iterator.next().replace(" ", "").split(";");
+                if (Integer.parseInt(columns[9]) < 25000)
+                    iterator.remove();
+                else
+                    newDatas.add(columns[6] + ";" + columns[9]);
+            }
+
+
+            Files.write(pathEcriture, newDatas);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
